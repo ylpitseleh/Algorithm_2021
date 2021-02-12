486402321eacf91f4b0cba1b2298f8941728eac3
이윤주 <leeyj8@brandi.co.kr>
2021-02-12 17:43:48 +0900

    Add 주식가격

---
+++ Programmers/Level2/주식가격.java
@@ -0,0 +1,26 @@
+package Programmers.Level2;
+/*
+PQ 쓰고 오만 방정 떨었는데 생각해보니 PQ 써서 푸는게 아니었다..
+"문제를 잘 읽자."
+스택 써서 해결하는건 이해를 못하겠다...흑흑...흑...
+*/
+
+public class 주식가격 {
+  public static void main(String[] args) {
+    int prices[] = {1, 2, 3, 2, 3};
+    int len = prices.length;
+    int answer[] = new int[prices.length];
+
+    for (int i=0; i<len; i++) {
+      int cnt = 0;
+      for (int j=i+1; j<len; j++) {
+        cnt++;
+        if (prices[i] > prices[j]) {
+          break ;
+        }
+      }
+      answer[i] = cnt;
+    }
+    // return answer;
+  }
+}
